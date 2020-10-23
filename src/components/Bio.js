b326d6b244037b33181d8f953e04ada84bf7dd6f
Inara Ramji <inara.ramji@gmail.com>
2020-10-23 21:54:21 +0100

    Add typing text animation

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,7 +1,9 @@
 import '../index.css';
+import '../Typist.scss';
 import React from "react";
 import "typeface-raleway";
 import Grid from "@material-ui/core/Grid";
+import Typist from 'react-typist';
 
 function Bio() {
     return (
@@ -13,7 +15,9 @@ function Bio() {
             style={{padding: '0px 20px'}}
         >
             <Grid item xs={12} style={{marginTop: '-10px', textAlign: 'center'}}>
-                <h1 style={{fontFamily: 'Raleway', fontWeight: 'bold', fontSize: '3em'}}>Hi, I'm Inara!</h1>
+                <h1 style={{fontFamily: 'Raleway', fontWeight: 'bold', fontSize: '3em'}}>
+                    <Typist cursor={{blink: true, hideWhenDone: true}}>Hi, I'm Inara!</Typist>
+                </h1>
             </Grid>
             <Grid item xs={12} style={{marginTop: '-40px', textAlign: 'center'}}>
                 <h3 style={{fontFamily: 'Raleway', fontWeight: 'normal'}}>I'm a software engineer at Google working on
